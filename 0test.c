7308ff7c7d05e9107d37ba73fd4b56bbfcfee817
Kwevan Gouacide <kwe@MacBook-Pro-de-Kwevan.local>
2019-12-22 21:29:27 +0100

    minor changes

---
+++ 0test.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("%%05s et 2: [%05s]\n", "ff"); //undef mais le result est normal 0 a gauche de la string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+**others
+*/
+
+	//printf("[*]" "     - arg: [ff] -\n");
+
+	// simple number %5s
+	
+/*	//printf("%%*s: [%*s]\n",  "ff"); //undef car pas de parametre pour * affiche plein despaces (maybe max_int) puis (null)
+	printf("%%*s et 0: [%*s]\n", 0, "ff");
+	printf("%%*s et 1: [%*s]\n", 1, "ff");
+	printf("%%*s et 2: [%*s]\n", 2, "ff");
+	printf("%%*s et 3: [%*s]\n", 3, "ff");
+	printf("%%*s et 4: [%*s]\n", 4, "ff");
+	printf("%%*s et 5: [%*s]\n", 5, "ff");
+	printf("\n");
+*/
+
+
+
+//	printf("%%**s et 3: [%**s]\n", 5, 20, "ff");//undef mais prend le deuxieme arg en compte
+//printf("%%************s et 3: [%*****s]\n", 5, 20, "ff");// war undef affiche chaine vie, a.out
+
+
+
+
+	//printf("%%*.*s et 5: [%*.*s]\n", 1, 5, "ff");
+	//printf("%%*.*s et 5: [%*.*s]\n", 5, 5, "ff");
+	//printf("%%*.*s et 5: [%*.*s]\n", 1, 1, "ff");
+//	printf("%%*.*s et 5: [%*.*s]\n", 5, 1, "ff"); //semble ignore ce quil y a apres le point
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//others
+
+	//0.0 - 0.5
+		
+	//undefined warnig it will just ignore the 0 and take the other flag in count
+/*	printf("%%0.0s: [%0.0s]\n", "ff");
+	printf("%%0.1s: [%0.1s]\n", "ff");
+	printf("%%0.2s: [%0.2s]\n", "ff");
+	printf("%%0.3s: [%0.3s]\n", "ff");
+	printf("%%0.4s: [%0.4s]\n", "ff");
+	printf("%%0.5s: [%0.5s]\n", "ff");
+	printf("\n");
+*/
+
+/*	// 1.0 - 1.5
+
+	printf("%%1.0s: [%1.0s]\n", "ff");
+	printf("%%1.1s: [%1.1s]\n", "ff");
+	printf("%%1.2s: [%1.2s]\n", "ff");
+	printf("%%1.3s: [%1.3s]\n", "ff");
+	printf("%%1.4s: [%1.4s]\n", "ff");
+	printf("%%1.5s: [%1.5s]\n", "ff");
+	printf("\n");
+*/
+
+/*
+	printf("%%.0s: [%.0s]\n", "ff");
+	printf("%%.1s: [%.1s]\n", "ff");
+	printf("%%.2s: [%.2s]\n", "ff");
+	printf("%%.3s: [%.3s]\n", "ff");
+	printf("%%.4s: [%.4s]\n", "ff");
+	printf("%%.5s: [%.5s]\n", "ff");
+
+	printf("\n");
+
+//	printf("%%0.s: [%0.s]\n", "ff");
+	printf("%%1.s: [%1.s]\n", "ff");
+	printf("%%2.s: [%2.s]\n", "ff");
+	printf("%%3.s: [%3.s]\n", "ff");
+	printf("%%4.s: [%4.s]\n", "ff");
+	printf("%%5.s: [%5.s]\n", "ff");
+
+
+	printf("\n");
+
+	printf("war %%0.0s: [%0.0s]\n", "ff");	//war undef
+	printf("war %%0.1s: [%0.1s]\n", "ff"); 	//same
+	printf("war %%0.2s: [%0.2s]\n", "ff");	//same
+	printf("war %%0.3s: [%0.3s]\n", "ff");	//same
+	printf("war %%0.5s: [%0.4s]\n", "ff");	//same
+	printf("war %%0.4s: [%0.5s]\n", "ff"); 	//war undef
+	printf("\n");
+
+	printf("%%1.0s: [%1.0s]\n", "ff"); 	
+	printf("%%1.1s: [%1.1s]\n", "ff"); 	
+	printf("%%1.2s: [%1.2s]\n", "ff"); 	
+	printf("%%1.3s: [%1.3s]\n", "ff"); 	
+	printf("%%1.4s: [%1.4s]\n", "ff"); 	
+	printf("%%1.5s: [%1.5s]\n", "ff"); 
+
+
+
+	printf("\n");
+
+	printf("\n\n     - arg: [66] -\n");
+	printf("%%1d: [%1d]\n", 55);
+	printf("%%2d: [%2d]\n", 55);
+	printf("%%3d: [%3d]\n", 55);
+	printf("%%4d: [%4d]\n", 55);
+	printf("%%5d: [%5d]\n", 55);
+
+	printf("\n");
+
+	printf("%%.0d: [%.0d]\n", 55);
+	printf("%%.1d: [%.1d]\n", 55);
+	printf("%%.2d: [%.2d]\n", 55);
+	printf("%%.3d: [%.3d]\n", 55);
+	printf("%%.4d: [%.4d]\n", 55);
+	printf("%%.5d: [%.5d]\n", 55);
+
+	printf("\n");
+	printf("%%0.d: [%.0d]\n", 55);
+	printf("%%1.d: [%.1d]\n", 55);
+	printf("%%2.d: [%.2d]\n", 55);
+	printf("%%3.d: [%.3d]\n", 55);
+	printf("%%5.d: [%.5d]\n", 55);
+
+*/
+
+}
